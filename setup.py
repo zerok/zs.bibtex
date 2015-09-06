718d096412cacbf8d6b046081f4713ba6b23d504
Horst Gutmann <zerok@zerokspot.com>
2015-09-06 10:42:08 +0200

    chore: Remove ez_setup and hg-related files

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from os.path import join, abspath, dirname
-try:
-    from setuptools import setup, find_packages
-except:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
         name='zs.bibtex',
